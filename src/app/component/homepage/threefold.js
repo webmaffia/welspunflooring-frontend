c92ba9c5b2b05d3c25a07aedcc1f6a300ac2e376
Javed <javed@marketinsider.in>
2024-11-12 12:16:42 +0530

    video guide

--- src/app/component/homepage/threefold.js
+++ src/app/component/homepage/threefold.js
@@ -1,22 +1,23 @@
 // components/EmiSection.js
 
 "use client"
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+
 const emiItems = [
   {
     img: 'images/emi/img-1.webp',
-    title: 'Apply for the EMI option',
-    description: 'Get some financing help to redo your space',
-    linkText: 'APPLY NOW',
-    linkHref: '#', // Add your link here
+    title: 'Budget Calculator',
+    description: 'Design your ideal floor, tailored to your style and Budget-Friendly Needs.',
+    linkText: 'Calculate Now',
+    linkHref: '/budget-calculator', // Add your link here
   },
-  {
-    img: 'images/emi/img-2.webp',
-    title: 'The Floor Finder',
-    description: 'Help us help you with the right kind of solution for your need',
-    linkText: 'GET STARTED',
-    linkHref: '#', // Add your link here
-  },
+  // {
+  //   img: 'images/emi/img-2.webp',
+  //   title: 'The Floor Finder',
+  //   description: 'Help us help you with the right kind of solution for your need',
+  //   linkText: 'GET STARTED',
+  //   linkHref: '#', // Add your link here
+  // },
   {
     img: 'images/emi/img-3.webp',
     title: 'Book an Expert Consultation',
@@ -27,9 +28,32 @@ const emiItems = [
 ];
 
 const EmiSection = () => {
+  useEffect(() => {
+    const handleChatClick = () => {
+      const chatBox = document.querySelector(".sticky_chat");
+      if (chatBox) {
+        chatBox.click();
+      }
+    };
+  
+    // Get elements by class name
+    const elements = document.getElementsByClassName("triggerChat");
+  
+    // Attach event listeners to each element
+    Array.from(elements).forEach((element) => {
+      element.addEventListener("click", handleChatClick);
+    });
+  
+    // Cleanup event listeners when component unmounts
+    return () => {
+      Array.from(elements).forEach((element) => {
+        element.removeEventListener("click", handleChatClick);
+      });
+    };
+  }, []);
     const [activeIndex, setActiveIndex] = useState(null);
   return (
-    <section className="emi" data-section="emi">
+    <section className="emi emi_two_items" data-section="emi">
       {emiItems.map((item, index) => (
         <div key={index}
         className={`emi_container cursor_img ${activeIndex === index ? 'active' : ''}`}
@@ -47,7 +71,13 @@ const EmiSection = () => {
             <h2 className="subtitle_60">{item.title.split('<br />').join(' ')}</h2>
             <p>{item.description}</p>
           </div>
-          <a href={item.linkHref} className="view_link cursor_cta purpleBg2">
+          <a
+  href={item.linkHref}
+  className={`view_link cursor_cta purpleBg2 ${item.linkHref.includes('#') ? 'triggerChat' : ''}`}
+>
+  {/* Link content here */}
+
+
             <div className="link_cta">
               <div className="arrow_bg">
                 <img
