ab5f0c2bce8a217e0dd74a1824ff2549ac0d2388
Javed <javed@marketinsider.in>
2024-11-15 18:32:40 +0530

    contact us

--- src/app/component/subproduct/details.js
+++ src/app/component/subproduct/details.js
@@ -28,6 +28,7 @@ const ProductDetail = ({ product, collection }) => {
       });
     };
   }, []);
+
   return (
     <section data-section="product_detail" className="product_detail">
       <div className="square_box square_box_15"></div>
@@ -36,20 +37,18 @@ const ProductDetail = ({ product, collection }) => {
           <ul>
             <li><Link href="/products">PRODUCTS</Link></li>
             <li>
-              <Link href={`/products/${product.attributes.category.data.attributes.product.data.attributes.slug}`}>
-                {product.attributes.category.data.attributes.product.data.attributes.product_name}
+              <Link href={`/products/${product?.attributes?.category?.data?.attributes?.product?.data?.attributes?.slug}`}>
+                {product?.attributes?.category?.data?.attributes?.product?.data?.attributes?.product_name}
               </Link>
             </li>
             <li>
-          
-            <Link href={`/products?category=${product.attributes.category.data.attributes.product.data.attributes.slug}&collection=${product.attributes.category.data.attributes.collectionName}`}>
-  {product.attributes.category.data.attributes.collectionName}
-</Link>
-
+              <Link href={`/products?category=${product?.attributes?.category?.data?.attributes?.product?.data?.attributes?.slug}&collection=${product?.attributes?.category?.data?.attributes?.collectionName}`}>
+                {product?.attributes?.category?.data?.attributes?.collectionName}
+              </Link>
             </li>
             <li>
-              <Link href={`/products/${product.attributes.category.data.attributes.product.data.attributes.slug}/${product.attributes.category.data.attributes.slug}/${product.attributes.slug}`}>
-                {product.attributes.subProductName}
+              <Link href={`/products/${product?.attributes?.category?.data?.attributes?.product?.data?.attributes?.slug}/${product?.attributes?.category?.data?.attributes?.slug}/${product?.attributes?.slug}`}>
+                {product?.attributes?.subProductName}
               </Link>
             </li>
           </ul>
@@ -57,12 +56,12 @@ const ProductDetail = ({ product, collection }) => {
         <div className="detail_product_content">
           <ProductSlides images={product} />
           <div className="about_our_product">
-            <h2 className="subtitle_5504">{product.attributes.subProductName}</h2>
+            <h2 className="subtitle_5504">{product?.attributes?.subProductName}</h2>
             <p>
-              {product.attributes.details.description.map((item, index) => (
+              {product?.attributes?.details?.description?.map((item, index) => (
                 <span key={index}>
-                  {item.children.map((child, childIndex) => (
-                    <span key={childIndex}>{child.text}</span>
+                  {item?.children?.map((child, childIndex) => (
+                    <span key={childIndex}>{child?.text}</span>
                   ))}
                 </span>
               ))}
@@ -70,28 +69,28 @@ const ProductDetail = ({ product, collection }) => {
             <div className="product_subContent">
               <div className="specified_links">
                 <Link className="opacity0" href="/view-space">VIEW IN YOUR SPACE</Link>
-                <Link href={`${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}${product.attributes?.details?.warranty?.data?.attributes?.url}`} target="_blank">WARRANTY</Link>
+                <Link href={`${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}${product?.attributes?.details?.warranty?.data?.attributes?.url}`} target="_blank">WARRANTY</Link>
                 <Link href="/locate-dealer">LOCATE DEALER</Link>
               </div>
               <div className="more_tiles">
                 <div className="tiles_view">
-                  <div className="tiles_name">More From {product.attributes.category.data.attributes.collectionName}</div>
-                  <Link href={`/products?category=${product.attributes.category.data.attributes.product.data.attributes.slug}&collection=${product.attributes.category.data.attributes.collectionName}`}>View More</Link>
+                  <div className="tiles_name">More From {product?.attributes?.category?.data?.attributes?.collectionName}</div>
+                  <Link href={`/products?category=${product?.attributes?.category?.data?.attributes?.product?.data?.attributes?.slug}&collection=${product?.attributes?.category?.data?.attributes?.collectionName}`}>View More</Link>
                 </div>
                 <div className="tiles_detail">
-                  {collection.slice(0, 14).map((tile, index) => (
-  <Link
-    key={index}
-    href={`/products/${product.attributes.category.data.attributes.product.data.attributes.slug}/${product.attributes.category.data.attributes.slug}/${tile.attributes.slug}`}
-  >
-    <Image
-      src={`${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}${tile.attributes.swatch.data.attributes.url}`}
-      alt={tile.attributes.swatch.data.attributes.name}
-      width={55}
-      height={55}
-    />
-  </Link>
-))}
+                  {collection?.slice(0, 14).map((tile, index) => (
+                    <Link
+                      key={index}
+                      href={`/products/${product?.attributes?.category?.data?.attributes?.product?.data?.attributes?.slug}/${product?.attributes?.category?.data?.attributes?.slug}/${tile?.attributes?.slug}`}
+                    >
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}${tile?.attributes?.swatch?.data?.attributes?.url}`}
+                        alt={tile?.attributes?.swatch?.data?.attributes?.name}
+                        width={55}
+                        height={55}
+                      />
+                    </Link>
+                  ))}
                 </div>
                 <div className="dFlex">
                   <Link href="#" className="view_link purpleBg triggerChat" id="makeInquery">
@@ -118,23 +117,22 @@ const ProductDetail = ({ product, collection }) => {
       </div>
 
       <div className="about_features">
-  {product.attributes.details.point?.map((item, index) => (
-    <div className="about_feature_box border_bg" key={index}>
-      <div className="border_diamond"></div>
-      <div className="border_right"></div>
-      <div className="subtitle_30">
-        {item.name?.map((point, index) => (
-          <span key={index}>
-            {point.children?.map((child, childIndex) => (
-              <span key={childIndex}>{child.text}</span>
-            ))}
-          </span>
+        {product?.attributes?.details?.point?.map((item, index) => (
+          <div className="about_feature_box border_bg" key={index}>
+            <div className="border_diamond"></div>
+            <div className="border_right"></div>
+            <div className="subtitle_30">
+              {item?.name?.map((point, pointIndex) => (
+                <span key={pointIndex}>
+                  {point?.children?.map((child, childIndex) => (
+                    <span key={childIndex}>{child?.text}</span>
+                  ))}
+                </span>
+              ))}
+            </div>
+          </div>
         ))}
       </div>
-    </div>
-  ))}
-</div>
-
     </section>
   );
 };
