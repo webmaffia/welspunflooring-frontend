d11b6957896683c6958884f5794febab1672b951
Javed <javed@marketinsider.in>
2024-11-15 17:54:15 +0530

    contact us

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -8,7 +8,7 @@ export default function contactUsPage() {
 			<section className="banner inner_banner">
     <picture>
         <source media="(max-width:540px)" srcset="/images/product/click_n_lock/banner_mob.webp" />
-        <img src="/images/product/click_n_lock/banner.webp" alt="" width="1920" height="472" />
+        <img src="/images/contact/Contact-Us-Banner.webp" alt="" width="1920" height="472" />
     </picture>
     <div className="inner_content">
         <h1 className="banner_heading">Contact Us</h1>
