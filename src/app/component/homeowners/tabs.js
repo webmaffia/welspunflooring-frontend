b3139cab0b948d866d6f130bfd1ad8f4ab277ca3
Javed <javed@marketinsider.in>
2025-02-05 16:29:51 +0530

    b2b added

--- src/app/component/homeowners/tabs.js
+++ src/app/component/homeowners/tabs.js
@@ -198,7 +198,7 @@ const ClickTiles = () => {
             />
           </picture>
           <div className="click_tiles_content">
-            <h2 className="subtitle_60">MultiStile</h2>
+            <h2 className="subtitle_60">MultiStile™ Tiles</h2>
             <p className="para">
             A great choice when you are looking to redo your flooring without compromising on aesthetics. MultiStile offers convenience as you can apply it within 24 hours*. It also allows for multi-surface application with benefits like durability, waterproof surface, scratch and stain resistance, and more. You can shortlist MultiStile for different spaces in your home: living room, bedrooms, kitchen, work from home rooms, etc. Welspun Flooring also helps out with expert installation to ease the process.
             </p>
