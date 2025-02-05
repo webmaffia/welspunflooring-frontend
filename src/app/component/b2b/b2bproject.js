f42c9252c92723f087741429e582f9bde3d27c9e
Javed <javed@marketinsider.in>
2025-02-05 12:34:01 +0530

    b2b added

--- src/app/component/b2b/b2bproject.js
+++ src/app/component/b2b/b2bproject.js
@@ -31,23 +31,23 @@ const B2BProject = () => {
     {
       image: "/images/b2b/new/Wall-to-wall.png",
       alt: "Wall to Wall",
-      title: "Wall to Wall",
+      title: "Wall to Wall Carpet",
       description:
         "Wall to Wall carpet flooring is designed with advanced printing and colour-fast technology that promises customisability and longevity when it comes to colour and texture. Mostly seen in hospitality, entertainment.",
       link: "/product/wall-to-wall-carpet"
     },
     {
       image: "/images/app/detail/img-1.webp",
-      alt: "Greens",
-      title: "Greens",
+      alt: "Purgloss Tiles",
+      title: "Purgloss Tiles",
       description:
-        "Just like real grass but with less maintenance and more durability, Welspun Flooring's Greens is a great option for vertical landscaping, designing recreational areas, and timeout zones in commercial spaces.",
-      link: "/product/artificial-grass"
+        "These are not limited to floors but can also be used on walls.  Its impressive glossy finish reflects light and helps dark rooms appear bright and compact spaces appear large.",
+      link: "/product/purgloss-tiles"
     },
     {
       image: "/images/b2b/new/Multistile.png",
       alt: "MultiStile",
-      title: "MultiStile™",
+      title: "MultiStile™ Tiles",
       description:
         "Introducing MultiStile™ flooring solutions for business spaces that offer customisable laying patterns and versatile multi-surface applications that add more value to the product, and spaces.",
       link: "/product/multistile"
